cec9d6eca1f1c876cbb16c997f02d8a130575829
Raheem A Omeiza <blesseth.omeiza@gmail.com>
2024-11-21 18:57:01 +0100

    reconfigured for deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,9 +6,9 @@ require('dotenv').config();
 export const PORT = process.env.PORT ? process.env.PORT : 5002;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const DB_NAME = process.env.DB_NAME || 'questionia';
-export const DATABASE_URL = 'mongodb://127.0.0.1:27017';//process.env.PORT
+export const DATABASE_URL = process.env.DATABASE_URL
   //@ts-ignore
-  // ? encodeURI(process.env.DATABASE_URL) : 'mongodb://127.0.0.1:27017';
+  ? encodeURI(process.env.DATABASE_URL) : 'mongodb://127.0.0.1:27017';
 export const BASE_URL = process.env.PORT ? process.env.BASE_URL : `http://localhost:${PORT}/`;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
   || 'iaowuebpiqupiwr8qb4pq39yrvwyveiwqpbrpiqy8y34yq377v5q45yy5';
